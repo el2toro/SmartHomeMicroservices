a104b4d2e82c58d5d3e56e8a7fb7da2c43329ef4
el2toro <vasea.badragan@gmail.com>
2025-03-08 19:29:24 +0100

    Implement GetDevices handler and endpoint

--- Services/DeviceManagement.API/Devices/GetDevices/GetDevicesEndpoint.cs
+++ Services/DeviceManagement.API/Devices/GetDevices/GetDevicesEndpoint.cs
@@ -1,6 +1,21 @@
-﻿namespace DeviceManagement.API.Devices.GetDevices
+﻿namespace DeviceManagement.API.Devices.GetDevices;
+
+//public record GetDevicesRequest();
+public record GetDevicesResponse(IEnumerable<object> Devices);
+public class GetDevicesEndpoint : ICarterModule
 {
-    public class GetDevicesEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/devices", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetDevicesQuery());
+            var response = result.Adapt<GetDevicesResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetDevices")
+        .Produces<GetDevicesResponse>(StatusCodes.Status200OK)
+        .WithDescription("Get Devices")
+        .WithSummary("Get Devices");
     }
 }
