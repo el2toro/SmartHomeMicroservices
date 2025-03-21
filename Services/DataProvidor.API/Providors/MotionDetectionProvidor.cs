0c403cb6644d35325314f19687bfe52cfc333b95
el2toro <vasea.badragan@gmail.com>
2025-03-21 21:46:18 +0100

    Add motion detection providor

---
+++ Services/DataProvidor.API/Providors/MotionDetectionProvidor.cs
@@ -0,0 +1,15 @@
+﻿namespace DataProvidor.API.Providors;
+
+public interface IMotionDetectionProvidor
+{
+    bool CheckMotionSensor();
+}
+
+public class MotionDetectionProvidor : IMotionDetectionProvidor
+{
+    public bool CheckMotionSensor()
+    {
+        Random random = new Random();
+        return random.Next(2) == 0;
+    }
+}
