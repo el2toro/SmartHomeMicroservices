278ebf65206a145c3852d85218a8e2e9fe57d999
el2toro <vasea.badragan@gmail.com>
2025-03-21 21:46:46 +0100

    Add motion detection event and handler

---
+++ Core/Core.Messaging/Events/MotionDetectedEvent.cs
@@ -0,0 +1,4 @@
+﻿namespace Core.Messaging.Events;
+
+public record MotionDetectedEvent(bool MotionDetected);
+
---
+++ Services/AutomationEngine.API/EventHandlers/MotionDetectedHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutomationEngine.API.Dtos;
+
+namespace AutomationEngine.API.EventHandlers;
+
+public class MotionDetectedHandler(IConfiguration configuration) : IConsumer<MotionDetectedEvent>
+{
+    private readonly IConfiguration _configuration = configuration;
+    public async Task Consume(ConsumeContext<MotionDetectedEvent> context)
+    {
+        var client = new HttpClient();
+        var mailEndpoint = _configuration["Endpoints:MailEndpoint"];
+
+        await client.PostAsJsonAsync(mailEndpoint, BuildMessage());
+    }
+
+    private MailDto BuildMessage()
+    {
+        return new MailDto("Motion detected", $"A motion was detected in the room at: {DateTime.Now}");
+    }
+}
