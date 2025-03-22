89323a348bb0b85bd02d0129e1299a32303dd475
el2toro <vasea.badragan@gmail.com>
2025-03-22 15:50:14 +0100

    Configure signalR

---
+++ Services/DeviceManagement.API/Hubs/DeviceHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DeviceManagement.API.Hubs;
+
+public class DeviceHub : Hub
+{
+    public async Task SendMessage(bool isOn)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", isOn);
+    }
+}
--- Services/DeviceManagement.API/Program.cs
+++ Services/DeviceManagement.API/Program.cs
@@ -2,6 +2,7 @@ using Core.Behaviors;
 using Core.Exceptions.Handler;
 using Core.Messaging.MassTransit;
 using DeviceManagement.API.Data.DbSettings;
+using DeviceManagement.API.Hubs;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,7 @@ builder.Services.AddMessageBroker(builder.Configuration, Assembly.GetExecutingAs
 builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddCarter();
 builder.Services.RegisterBsonSerializer();
+builder.Services.AddSignalR();
 
 builder.Services.AddSingleton<MongoDbContext>();
 builder.Services.AddScoped<IDeviceData, DeviceData>();
@@ -27,11 +29,23 @@ builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("Mo
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+        builder.AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials()
+               .WithOrigins("http://localhost:4200"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+
+app.MapHub<DeviceHub>("/deviceNotification");
 
 app.Run();
 
